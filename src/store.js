ebdcf57fe43fa77735394e291506220f718411fe
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2021-02-07 18:27:19 -0300

    Add all store mutations

--- src/store.js
+++ src/store.js
@@ -6,26 +6,28 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: { contacts: [] },
-  getters: {},
+  getters: {
+    allContacts: (state) => state.contacts,
+  },
   actions: {
     getContacts({ commit }) {
       return api
         .get(`/contacts`)
-        .then(({ data }) => commit("GET_CONTACTS", data.data))
+        .then(({ data }) => commit("GET_CONTACTS", data))
         .catch((err) => console.log(err))
     },
 
     createContact({ commit }, contactData) {
       return api
         .post(`/contacts`, { ...contactData })
-        .then(({ data }) => commit("CREATE_CONTACT", data.data))
+        .then(({ data }) => commit("CREATE_CONTACT", data))
         .catch((err) => console.log(err))
     },
 
     updateContact({ commit }, id, contactData) {
       return api
         .put(`/contacts/${id}`, { ...contactData })
-        .then(({ data }) => commit("UPDATE_CONTACT", data.data))
+        .then(({ data }) => commit("UPDATE_CONTACT", data))
         .catch((err) => console.log(err))
     },
 
@@ -37,8 +39,25 @@ const store = new Vuex.Store({
     },
   },
   mutations: {
-    GET_CONTACTS(state, contacts) {
-      state.contacts = contacts
+    GET_CONTACTS(state, payload) {
+      state.contacts = payload.data
+    },
+    CREATE_CONTACT(state, payload) {
+      state.contacts = [...state, payload.data]
+    },
+    UPDATE_CONTACT(state, payload) {
+      state.contacts = state.map((contact) => {
+        if (contact.id === payload.data.id) {
+          return payload.data
+        } else {
+          return contact
+        }
+      })
+    },
+    DELETE_CONTACT(state, payload) {
+      state.contacts = state.contacts.filter(
+        (contact) => contact.id !== payload
+      )
     },
   },
 })
